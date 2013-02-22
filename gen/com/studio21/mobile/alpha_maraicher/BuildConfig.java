f47d06669a5aa4827a97fbaab984b095486c2de1
Daniel Carrier <danielcarrier21@gmail.com>
2013-02-22 08:22:05 -0500

    Premiers fichiers

---
+++ gen/com/studio21/mobile/alpha_maraicher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.studio21.mobile.alpha_maraicher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
